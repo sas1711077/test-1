92cc5296047c47eb46fb70dee2931defd30961d9
sas1711077 <105028906+sas1711077@users.noreply.github.com>
2022-05-06 10:15:32 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
 	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+    basic.showNumber(0)
+    basic.pause(500)
+    basic.showNumber(1)
+    basic.pause(100)
 })
